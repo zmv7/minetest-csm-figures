78a43f7f3e3ee52e73df0a2801c373136c58ed84
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-06-15 00:37:35 +0500

    Configurable parameters

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local pdraw_distance = 15
+local s = core.get_mod_storage()
 
 local poses = {}
 local cwp
@@ -12,7 +12,7 @@ function hs(center,radius,dome)
 			for z = minp.z, maxp.z do
 				local pos = vector.new(x, y, z)
 				local distance = vector.distance(center, pos)
-				local node = core.get_node_or_nil(pos)
+				local node = s:get("nodecheck") == "true" and core.get_node_or_nil(pos)
 				if distance >= radius - 1 and distance <= radius and (not node or node.name == "air") then
 					if not dome or pos.y >= center.y then
 						table.insert(poses,pos)
@@ -28,19 +28,22 @@ core.register_globalstep(function(dtime)
 		local ppos = core.localplayer:get_pos()
 		for _,pos in ipairs(poses) do
 			local distance = vector.distance(ppos, pos)
-			if distance < pdraw_distance then
-				core.add_particle({
-					pos = pos,
-					velocity = {x=0, y=0, z=0},
-					acceleration = {x=0, y=0, z=0},
-					expirationtime = dtime,
-					size = 10,
-					collisiondetection = false,
-					collision_removal = false,
-					vertical = false,
-					texture = "cdb_add.png",
-					glow = 14
-				})
+			if distance <= (s:get_int("draw_distance") or 15) then
+				local node = s:get("nodecheck") == "true" and core.get_node_or_nil(pos)
+				if not node or node.name == "air" then
+					core.add_particle({
+						pos = pos,
+						velocity = {x=0, y=0, z=0},
+						acceleration = {x=0, y=0, z=0},
+						expirationtime = dtime,
+						size = 10,
+						collisiondetection = false,
+						collision_removal = false,
+						vertical = false,
+						texture = "cdb_add.png",
+						glow = 14
+					})
+				end
 			end
 		end
 	end
@@ -60,12 +63,12 @@ core.register_chatcommand("sphere",{
 		local here = vector.round(core.localplayer:get_pos())
 		cwp = core.localplayer:hud_add({
 			hud_elem_type = "waypoint",
-			name = "Sphere center",
+			name = "Sphere center "..core.pos_to_string(here),
 			number = 0xFF0000,
 			world_pos = here
 		})
 		hs(here,radius)
-		return true, "Sphere r="..param.." created"
+		return true, "Sphere r="..param.." created, "..#poses.." positions allocated"
 	end
 	return true, "Sphere disabled"
 end})
@@ -83,12 +86,39 @@ core.register_chatcommand("dome",{
 		local here = vector.round(core.localplayer:get_pos())
 		cwp = core.localplayer:hud_add({
 			hud_elem_type = "waypoint",
-			name = "Dome center",
+			name = "Dome center "..core.pos_to_string(here),
 			number = 0xFF0000,
 			world_pos = here
 		})
 		hs(here,radius,true)
-		return true, "Dome r="..param.." created"
+		return true, "Dome r="..param.." created, "..#poses.." positions allocated"
 	end
 	return true, "Dome disabled"
 end})
+
+core.register_chatcommand("sphereconf",{
+  description = "Configure sphere assistant",
+  params = "<radius>",
+  func = function(param)
+	core.show_formspec("sphereconf",
+		"size[3,3.2]" ..
+		"label[0.7,0;SphereConfig]" ..
+		"field[0.4,1.2;2.7,1;draw_distance;Draw distance;"..(s:get("draw_distance") or 15).."]" ..
+		"field_close_on_enter[draw_distance;false]" ..
+		"checkbox[0.1,1.5;nodecheck;Draw only on air;"..(s:get("nodecheck") or "false").."]" ..
+		"button_exit[0.1,2.2;2.8,1;done;Done]"
+	)
+end})
+
+core.register_on_formspec_input(function(formname,fields)
+	if formname ~= "sphereconf" then return end
+	if fields.nodecheck then
+		s:set_string("nodecheck",fields.nodecheck)
+	end
+	if fields.done or fields.key_enter then
+		local dd = tonumber(fields.draw_distance)
+		if dd then
+			s:set_int("draw_distance",dd)
+		end
+	end
+end)
