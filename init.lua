7ec3a166effdece03d03e998363e903eb6adb655
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-10-27 17:26:34 +0500

    Add .ring

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local s = core.get_mod_storage()
 local poses = {}
 local cwp
 
-function hs(center,radius,dome)
+local function hs(center,radius,dome)
 	if not (center and radius) then return end
 	local minp = vector.subtract(center, vector.new(radius, radius, radius))
 	local maxp = vector.add(center, vector.new(radius, radius, radius))
@@ -15,6 +15,25 @@ function hs(center,radius,dome)
 				local node = s:get("nodecheck") == "true" and core.get_node_or_nil(pos)
 				if distance >= radius - 1 and distance <= radius and (not node or node.name == "air") then
 					if not dome or pos.y >= center.y then
+						table.insert(poses,pos)
+					end
+				end
+			end
+		end
+	end
+end
+
+local function ring(center, radius, axis)
+	local minp = vector.subtract(center, vector.new(radius, radius, radius))
+	local maxp = vector.add(center, vector.new(radius, radius, radius))
+	for x = minp.x, maxp.x do
+		for y = minp.y, maxp.y do
+			for z = minp.z, maxp.z do
+				local pos = vector.new(x, y, z)
+				local distance = vector.distance(center, pos)
+				local node = s:get("nodecheck") == "true" and core.get_node_or_nil(pos)
+				if distance >= radius - 1 and distance <= radius and (not node or node.name == "air") then
+					if pos[axis] == center[axis] then
 						table.insert(poses,pos)
 					end
 				end
@@ -48,6 +67,40 @@ core.register_globalstep(function(dtime)
 		end
 	end
 end)
+
+core.register_chatcommand("ring",{
+  description = "Create ring of particles",
+  params = "<radius> [axis]",
+  func = function(param)
+	poses = {}
+	if cwp then
+		core.localplayer:hud_remove(cwp)
+		cwp = nil
+	end
+	local radius, axis = param:match("(%d+) (.+)")
+	if not (radius and axis) then
+		radius = param
+		axis = "y"
+	end
+	radius = tonumber(radius)
+	if radius then
+		axis = axis:lower()
+		if axis ~= "x" and axis ~= "y" and axis ~= "z" then
+			return false, "Invalid axis"
+		end
+		local pos = vector.round(core.localplayer:get_pos())
+		ring(pos,radius,axis)
+		cwp = core.localplayer:hud_add({
+			hud_elem_type = "waypoint",
+			name = "Ring center "..core.pos_to_string(pos),
+			number = 0xFF0000,
+			world_pos = pos
+		})
+		return true, "Ring created"
+	else
+		return false, "Ring disabled"
+	end
+end})
 
 core.register_chatcommand("sphere",{
   description = "Create hollow sphere of particles",
@@ -96,12 +149,12 @@ core.register_chatcommand("dome",{
 	return true, "Dome disabled"
 end})
 
-core.register_chatcommand("sphereconf",{
-  description = "Configure sphere assistant",
+core.register_chatcommand("figuresconf",{
+  description = "Configure figures assistant",
   func = function(param)
-	core.show_formspec("sphereconf",
+	core.show_formspec("figuresconf",
 		"size[3,3.2]" ..
-		"label[0.7,0;SphereConfig]" ..
+		"label[0.7,0;FiguresConfig]" ..
 		"field[0.4,1.2;2.7,1;draw_distance;Draw distance;"..(s:get("draw_distance") or 15).."]" ..
 		"field_close_on_enter[draw_distance;false]" ..
 		"checkbox[0.1,1.5;nodecheck;Draw only on air;"..(s:get("nodecheck") or "false").."]" ..
@@ -110,7 +163,7 @@ core.register_chatcommand("sphereconf",{
 end})
 
 core.register_on_formspec_input(function(formname,fields)
-	if formname ~= "sphereconf" then return end
+	if formname ~= "figuresconf" then return end
 	if fields.nodecheck then
 		s:set_string("nodecheck",fields.nodecheck)
 	end
